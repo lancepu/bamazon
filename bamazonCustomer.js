66d80077ce403610265640f1064467c65c4e1706
LanFP888 <41882036+LanFP888@users.noreply.github.com>
2018-12-21 18:21:05 -0500

    complete purchase component

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -83,4 +83,47 @@ function getItemListing() {
   });
 }
 
-module.exports.purchase = purchase;
+// Main Sequence
+function init() {
+  inquirer
+    .prompt([
+      {
+        name: "choice",
+        type: "list",
+        choices: ["Purchase", "Manager Login", "Supervisor Login", "Quit"],
+        message: "Welcome to bamazon: what do you like to do?"
+      }
+    ])
+    .then(answer => {
+      switch (answer.choice) {
+        case "Purchase":
+          purchase();
+          break;
+        case "Manager Login":
+          managerPortal();
+          break;
+        case "Supervisor Login":
+          supPortal();
+          break;
+        default:
+          doQuit();
+      }
+    });
+}
+
+function managerPortal() {
+  console.log("manager portal");
+  //options; login and quit
+}
+
+function supPortal() {
+  console.log("supervisor portal");
+  //options; login and quit
+}
+
+function doQuit() {
+  connection.end();
+  console.log("Good Bye!");
+}
+
+init();
